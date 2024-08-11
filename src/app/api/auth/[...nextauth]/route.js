000a40abb93e707fffe11668cc3e8f83e5513eea
Rabil <96547109+Rabil-Ibraiel@users.noreply.github.com>
2024-08-12 02:15:22 +0300

    Deploy Update 3

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
-  secrt: process.env.AUTH_SECRET,
+  secrt: process.env.NEXTAUTH_SECRET,
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
